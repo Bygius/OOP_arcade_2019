a79793f2044ac881528fa425fdb31b5e0dff2c6a
Peter Bessone <peter.bessone@epitech.eu>
2020-03-31 23:32:58 +0200

    FIX - makefile

---
+++ games/game_pacman/include/Pacman.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2020
+** peter bessone
+** File description:
+** Pacman
+*/
+
+#ifndef PACMAN_HPP_
+#define PACMAN_HPP_
+
+#include "Arcade_interfaces.hpp"
+
+class Pacman : public IGameModule
+{
+    private:
+        int x1;
+        int y1;
+        IDisplayModule &_lib;
+    public:
+        Pacman(IDisplayModule &);
+        virtual ~Pacman() = default;
+
+        void reset();
+        bool loadFromFile(const std::string &filepath);
+        bool loadFromFile();
+        bool saveToFile(const std::string &filepath) const;
+        bool saveToFile() const;
+        void setPlayerName(const std::string &name);
+        std::pair<std::string, int> getScore() const;
+        std::vector<std::pair<std::string, int>> getBestScores() const;
+        void update(const IDisplayModule &lib);
+        void render(IDisplayModule &lib) const;
+        const std::string &getLibName() const;
+};
+
+#endif /* !Pacman_HPP_ */
